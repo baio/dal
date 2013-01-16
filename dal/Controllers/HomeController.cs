031f4112f66b974a8273561a0fd876b25388968d
baio <max.putilov@gmail.com>
2013-01-16 22:23:30 +0400

    DAL-7, show versions

--- dal/Controllers/HomeController.cs
+++ dal/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dal.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +7,7 @@ using System.Web.Mvc;
 
 namespace dal.Controllers
 {
-    
+    [VersionFilterAttribute]
     public class HomeController : Controller
     {
         public ActionResult Index()
--- dal/Controllers/ProjectController.cs
+++ dal/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
-﻿using dal.Models;
+﻿using dal.Filters;
+using dal.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Web.Mvc;
 
 namespace dal.Controllers
 {
+    [VersionFilterAttribute]
     public class ProjectController : Controller
     {
         
